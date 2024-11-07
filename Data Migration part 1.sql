3581f361f521098d69b7da0bdb924cc49c807b99
Kelvindr09 <oduwalekelvin@gmail.com>
2024-11-07 08:23:09 -0500

    Add files via upload

---
+++ Data Migration part 1.sql
@@ -0,0 +1,444 @@
+use SQLTraining
+
+--ASSIGNMENT
+--DATA MIGRATION
+
+--NOBLE
+select * into Dev.dbo.Noble
+from SQLTraining.dbo.Noble
+
+select * into Test.dbo.Noble
+from SQLTraining.dbo.Noble
+
+select * into Prod.dbo.Noble
+from SQLTraining.dbo.Noble
+
+--GAMES
+select * into Dev.dbo.Games
+from SQLTraining.dbo.Games
+
+select * into Test.dbo.Games
+from SQLTraining.dbo.Games
+
+select * into Prod.dbo.Games
+from SQLTraining.dbo.Games
+
+
+--WORLD
+select * into Dev.dbo.World
+from SQLTraining.dbo.World
+
+select * into Test.dbo.World
+from SQLTraining.dbo.World
+
+select * into Prod.dbo.World
+from SQLTraining.dbo.World
+
+
+--MOVIES
+select * into Dev.dbo.Movies
+from SQLTraining.dbo.Movies
+
+select * into Test.dbo.Movies
+from SQLTraining.dbo.Movies
+
+select * into Prod.dbo.Movies
+from SQLTraining.dbo.Movies
+
+
+--FRUITS
+select * into Dev.dbo.Fruits
+from SQLTraining.dbo.Fruits
+
+select * into Test.dbo.Fruits
+from SQLTraining.dbo.Fruits
+
+select * into Prod.dbo.Fruits
+from SQLTraining.dbo.Fruits
+
+
+--INSTRUCTOR
+select * into Dev.dbo.Instructor
+from SQLTraining.dbo.Instructor
+
+select * into Test.dbo.Instructor
+from SQLTraining.dbo.Instructor
+
+select * into Prod.dbo.Instructor
+from SQLTraining.dbo.Instructor
+
+
+--EMPLOYEES
+select * into Dev.dbo.Employees
+from SQLTraining.dbo.Employees
+
+select * into Test.dbo.Employees
+from SQLTraining.dbo.Employees
+
+select * into Prod.dbo.Employees
+from SQLTraining.dbo.Employees
+
+*
+
+--COUNTRY_DATA
+select * into Dev.dbo.Country_data
+from SQLTraining.dbo.Country_data
+
+select * into Test.dbo.Country_data
+from SQLTraining.dbo.Country_data
+
+select * into Prod.dbo.Country_data
+from SQLTraining.dbo.Country_data
+
+
+--COVID_DATA
+select * into Dev.dbo.Covid_data
+from SQLTraining.dbo.Covid_data
+
+select * into Test.dbo.Covid_data
+from SQLTraining.dbo.Covid_data
+
+select * into Prod.dbo.Covid_data
+from SQLTraining.dbo.Covid_data
+
+
+--CUSTOMERINFO 
+select * into Dev.dbo.CustomerInfo
+from SQLTraining.dbo.CustomerInfo
+
+select * into Test.dbo.Customerinfo
+from SQLTraining.dbo.CustomerInfo
+
+select * into Prod.dbo.CustomerInfo
+from SQLTraining.dbo.CustomerInfo
+
+
+--DEALER_CAR
+select * into Dev.dbo.Dealer_Car
+from SQLTraining.dbo.Dealer_Car
+
+select * into Test.dbo.Dealer_Car
+from SQLTraining.dbo.Dealer_Car
+
+select * into Prod.dbo.Dealer_Car
+from SQLTraining.dbo.Dealer_Car
+
+
+--DEALER_CUSTOMER
+select * into Dev.dbo.Dealer_Customer
+from SQLTraining.dbo.Dealer_Customer
+
+select * into Test.dbo.Dealer_Customer
+from SQLTraining.dbo.Dealer_Customer
+
+select * into Prod.dbo.Dealer_Customer
+from SQLTraining.dbo.Dealer_Customer
+
+
+--COVID_DATA_AFRICA
+select * into Dev.dbo.Covid_data_Africa
+from SQLTraining.dbo.Covid_data_Africa
+
+select * into Test.dbo.Covid_data_Africa
+from SQLTraining.dbo.Covid_data_Africa
+
+select * into Prod.dbo.Covid_data_Africa
+from SQLTraining.dbo.Covid_data_Africa
+
+
+
+--POPULATION_DATA
+select * into Dev.dbo.Population_data
+from SQLTraining.dbo.Population_data
+
+select * into Test.dbo.Population_data
+from SQLTraining.dbo.Population_data
+
+select * into Prod.dbo.Population_data
+from SQLTraining.dbo.Population_data
+
+
+--WALMART_SALES_DATA
+select * into Dev.dbo.Walmart_Sales_Data
+from SQLTraining.dbo.Walmart_Sales_Data
+
+select * into Test.dbo.Walmart_Sales_Data
+from SQLTraining.dbo.Walmart_Sales_Data
+
+select * into Prod.dbo.Walmart_Sales_Data
+from SQLTraining.dbo.Walmart_Sales_Data
+
+
+--SALESDATA1
+select * into Dev.dbo.SalesData1
+from SQLTraining.dbo.SalesData1
+
+select * into Test.dbo.SalesData1
+from SQLTraining.dbo.SalesData1
+
+select * into Prod.dbo.SalesData1
+from SQLTraining.dbo.SalesData1
+
+
+--REGIONAL_SALES
+select * into Dev.dbo.Regional_Sales
+from SQLTraining.dbo.Regional_Sales
+
+select * into Test.dbo.Regional_Sales
+from SQLTraining.dbo.Regional_Sales
+
+select * into Prod.dbo.Regional_Sales
+from SQLTraining.dbo.Regional_Sales
+
+
+--REGIONAL_SALES_AFRICA
+select * into Dev.dbo.Regional_Sales_Africa
+from SQLTraining.dbo.Regional_Sales_Africa
+
+select * into Test.dbo.Regional_Sales_Africa
+from SQLTraining.dbo.Regional_Sales_Africa
+
+select * into Prod.dbo.Regional_Sales_Africa
+from SQLTraining.dbo.Regional_Sales_Africa
+
+
+--REGIONAL_SALES_ASIA
+select * into Dev.dbo.Regional_Sales_Asia
+from SQLTraining.dbo.Regional_Sales_Asia
+
+select * into Test.dbo.Regional_Sales_Asia
+from SQLTraining.dbo.Regional_Sales_Asia
+
+select * into Prod.dbo.Regional_Sales_Asia
+from SQLTraining.dbo.Regional_Sales_Asia
+
+
+--REGIONAL_SALES_EUROPE
+select * into Dev.dbo.Regional_Sales_Europe
+from SQLTraining.dbo.Regional_Sales_Europe
+
+select * into Test.dbo.Regional_Sales_Europe
+from SQLTraining.dbo.Regional_Sales_Europe
+
+select * into Prod.dbo.Regional_Sales_Europe
+from SQLTraining.dbo.Regional_Sales_Europe
+
+
+----REGIONAL_SALES_NORTHAMERICA
+select * into Dev.dbo.Regional_Sales_NorthAmerica
+from SQLTraining.dbo.Regional_Sales_NorthAmerica
+
+select * into Test.dbo.Regional_Sales_NorthAmerica
+from SQLTraining.dbo.Regional_Sales_NorthAmerica
+
+select * into Prod.dbo.Regional_Sales_NorthAmerica
+from SQLTraining.dbo.Regional_Sales_NorthAmerica
+
+
+--NEW_REGIONAL_SALES
+select * into Dev.dbo.New_Regional_Sales
+from SQLTraining.dbo.New_Regional_Sales
+
+select * into Test.dbo.New_Regional_Sales
+from SQLTraining.dbo.New_Regional_Sales
+
+select * into Prod.dbo.New_Regional_Sales
+from SQLTraining.dbo.New_Regional_Sales
+
+
+--EMP
+select * into Dev.dbo.EMP
+from SQLTraining.dbo.EMP
+
+select * into Test.dbo.EMP
+from SQLTraining.dbo.EMP
+
+select * into Prod.dbo.EMP
+from SQLTraining.dbo.EMP
+
+
+--DAX_SALES
+select * into Dev.dbo.DAX_Sales
+from SQLTraining.dbo.DAX_Sales
+
+select * into Test.dbo.DAX_Sales
+from SQLTraining.dbo.DAX_Sales
+
+select * into Prod.dbo.DAX_Sales
+from SQLTraining.dbo.DAX_Sales
+
+
+*--SALES_DATA
+select * into Dev.dbo.Sales_data
+from SQLTraining.dbo.Sales_data
+
+select * into Test.dbo.Sales_data
+from SQLTraining.dbo.Sales_data
+
+select * into Prod.dbo.Sales_data
+from SQLTraining.dbo.Sales_data
+
+
+
+
+use SQLTrainingCase
+--BUSINESS
+select * into Dev.dbo.Business
+from SQLTrainingCase.dbo.Business
+
+select * into Test.dbo.Business
+from SQLTrainingCase.dbo.Business
+
+select * into Prod.dbo.Business
+from SQLTrainingCase.dbo.Business
+
+
+--COMPANYINFO
+select * into Dev.dbo.Companyinfo
+from SQLTrainingCase.dbo.Companyinfo
+
+select * into Test.dbo.Companyinfo
+from SQLTrainingCase.dbo.Companyinfo
+
+select * into Prod.dbo.Companyinfo
+from SQLTrainingCase.dbo.Companyinfo
+
+
+--EMPLOYEEINFO
+select * into Dev.dbo.EmployeeInfo
+from SQLTrainingCase.dbo.EmployeeInfo
+
+select * into Test.dbo.EmployeeInfo
+from SQLTrainingCase.dbo.EmployeeInfo
+
+select * into Prod.dbo.EmployeeInfo
+from SQLTrainingCase.dbo.EmployeeInfo
+
+
+--PET
+select * into Dev.dbo.Pet
+from SQLTrainingCase.dbo.Pet
+
+select * into Test.dbo.Pet
+from SQLTrainingCase.dbo.Pet
+
+select * into Prod.dbo.Pet
+from SQLTrainingCase.dbo.Pet
+
+
+--PRSN
+select * into Dev.dbo.PRSN
+from SQLTrainingCase.dbo.PRSN
+
+select * into Test.dbo.PRSN
+from SQLTrainingCase.dbo.PRSN
+
+select * into Prod.dbo.PRSN
+from SQLTrainingCase.dbo.PRSN
+
+
+--PRSN_ADDR
+select * into Dev.dbo.PRSN_ADDR
+from SQLTrainingCase.dbo.PRSN_ADDR
+
+select * into Test.dbo.PRSN_ADDR
+from SQLTrainingCase.dbo.PRSN_ADDR
+
+select * into Prod.dbo.PRSN_ADDR
+from SQLTrainingCase.dbo.PRSN_ADDR
+
+
+--PRSN_PHN
+select * into Dev.dbo.PRSN_PHN
+from SQLTrainingCase.dbo.PRSN_PHN
+
+select * into Test.dbo.PRSN_PHN
+from SQLTrainingCase.dbo.PRSN_PHN
+
+select * into Prod.dbo.PRSN_PHN
+from SQLTrainingCase.dbo.PRSN_PHN
+
+
+--STUDENT
+select * into Dev.dbo.Student
+from SQLTrainingCase.dbo.Student
+
+select * into Test.dbo.Student
+from SQLTrainingCase.dbo.Student
+
+select * into Prod.dbo.Student
+from SQLTrainingCase.dbo.Student
+
+
+
+
+
+use SQLinterview
+--EMPLOYEE
+select * into Dev.dbo.Employee
+from SQLinterview.dbo.Employee
+
+select * into Test.dbo.Employee
+from SQLinterview.dbo.Employee
+
+select * into Prod.dbo.Employee
+from SQLinterview.dbo.Employee
+
+
+--JOB
+select * into Dev.dbo.Job
+from SQLinterview.dbo.Job
+
+select * into Test.dbo.Job
+from SQLinterview.dbo.Job
+
+select * into Prod.dbo.Job
+from SQLinterview.dbo.Job
+
+
+--LOCATION
+select * into Dev.dbo.Location
+from SQLinterview.dbo.Location
+
+select * into Test.dbo.Location
+from SQLinterview.dbo.Location
+
+select * into Prod.dbo.Location
+from SQLinterview.dbo.Location
+
+
+
+
+use SQLtrainingunion
+--NOBLEUNION
+select * into Dev.dbo.NobleUnion
+from SQLtrainingunion.dbo.NobleUnion
+
+select * into Test.dbo.NobleUnion
+from SQLtrainingunion.dbo.NobleUnion
+
+select * into Prod.dbo.NobleUnion
+from SQLtrainingunion.dbo.NobleUnion
+
+
+
+--NOBLEUNIONBKUP
+select * into Dev.dbo.NobleUnionBkup
+from SQLtrainingunion.dbo.NobleUnionBkup
+
+select * into Test.dbo.NobleUnionBkup
+from SQLtrainingunion.dbo.NobleUnionBkup
+
+select * into Prod.dbo.NobleUnionBkup
+from SQLtrainingunion.dbo.NobleUnionBkup
+
+
+--NODATA
+select * into Dev.dbo.NoData
+from SQLtrainingunion.dbo.NoData
+
+select * into Test.dbo.NoData
+from SQLtrainingunion.dbo.NoData
+
+select * into Prod.dbo.NoData
+from SQLtrainingunion.dbo.NoData
